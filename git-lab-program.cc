075e89cf4748c91fe156a6638eebece4e52007f8
Hope Bowden <hb295315@ohio.edu>
2020-05-14 14:07:10 -0400

    New Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hope Bowden
+ *        Date: 4/15/2020
+ * Description: Simple hello git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
